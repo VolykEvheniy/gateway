64c7d837ab1d29ca6663bde213af3fa252c82ebf
volyk_evheniy <evhenii_official@gmail.com>
2024-07-02 01:54:33 +0300

    replaced postgresql db instead of h2

--- src/main/java/com/vlkevheniy/gateway/model/UserSession.java
+++ src/main/java/com/vlkevheniy/gateway/model/UserSession.java
@@ -1,15 +1,15 @@
 package com.vlkevheniy.gateway.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.Instant;
 
 @Getter
 @Setter
-@Entity
+@Table(name = "user_session")
 public class UserSession {
 
     @Id
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS user_session (
     id VARCHAR(50) PRIMARY KEY,
     email VARCHAR(50),
     name VARCHAR(100),
-    expires_at DATETIME
+    expires_at TIMESTAMP
 );
